12393a554a657b33ba1fda8ecdb07151c6ff50dd
ibrahim89 <hafizeabdullah@gmail.com>
2022-07-11 20:19:01 +0530

    Updates

---
+++ precache-manifest.b95b88d8388b42a64db616d03f8b80dc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5207e6af1d77a83ca37e40022b1bdfec",
+    "url": "/admin-ui-app/index.html"
+  },
+  {
+    "revision": "8d740442b3632bd3f5f7",
+    "url": "/admin-ui-app/static/css/2.13f24655.chunk.css"
+  },
+  {
+    "revision": "b2a00e925b99a3d40c3c",
+    "url": "/admin-ui-app/static/css/main.65178c65.chunk.css"
+  },
+  {
+    "revision": "8d740442b3632bd3f5f7",
+    "url": "/admin-ui-app/static/js/2.a6f27edf.chunk.js"
+  },
+  {
+    "revision": "b2a00e925b99a3d40c3c",
+    "url": "/admin-ui-app/static/js/main.eb7021ad.chunk.js"
+  },
+  {
+    "revision": "614f72e88f6d352f7906",
+    "url": "/admin-ui-app/static/js/runtime~main.0619bf86.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/admin-ui-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/admin-ui-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/admin-ui-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/admin-ui-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/admin-ui-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
